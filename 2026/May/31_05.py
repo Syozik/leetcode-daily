2854c602847a0356338cde0a2c7955f00d6ccf14
Serhii Rubanskyi <seru@odoo.com>
2026-06-01 14:11:02 +0200

    [ADD] 31.05 problem

---
+++ 2026/May/31_05.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/destroying-asteroids/description/?envType=daily-question&envId=2026-06-01
+
+# You are given an integer mass, which represents the original mass of a planet.
+# You are further given an integer array asteroids, where asteroids[i] is the mass
+# of the ith asteroid.
+
+# You can arrange for the planet to collide with the asteroids in any arbitrary order.
+# If the mass of the planet is greater than or equal to the mass of the asteroid, the
+# asteroid is destroyed and the planet gains the mass of the asteroid. Otherwise, the
+# planet is destroyed.
+
+# Return true if all asteroids can be destroyed. Otherwise, return false.
+
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        for asteroid in asteroids:
+            if mass < asteroid:
+                return False
+            mass += asteroid
+        return True
+
+# <Medium> Array, Greedy, Sorting
+# Runtime 71ms 73.97%
+# Memory 33.81MB 75.93%
+
